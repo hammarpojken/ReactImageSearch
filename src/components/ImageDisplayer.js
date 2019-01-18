cbdefae91c21e68e835c1c6a2461dac32cc09ff5
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-18 15:55:05 +0100

    Create component for each image

--- src/components/ImageDisplayer.js
+++ src/components/ImageDisplayer.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "./ImageDisplayer.css";
+import ImageCard from "./ImageCard";
 
 const ImageDisplayer = props => {
-  const a = props.images.map((image, i) => {
-    return <img key={i} alt={image.description} src={image.urls.small} />;
+  const a = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{a}</div>;
+  return <div className="image-list">{a}</div>;
 };
 
 export default ImageDisplayer;
